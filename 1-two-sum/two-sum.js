a8ef137bf9ba62f4f4d83b307c9c9538fbf2928c
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-02-17 08:13:10 +0200

    Time: 60 ms (71.19%) | Memory: 51.7 MB (8.19%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,11 +4,14 @@
  * @return {number[]}
  */
 const twoSum = function(nums, target) {
-    for(let i=0; i<nums.length;i++){
-        const diff = target - nums[i];
-        const b = nums.slice(i+1).find(num=>diff === num);
-        if(typeof b === 'number'){
-        return [i,nums.slice(i+1).indexOf(b)+i+1];
-        } continue
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const value = nums[i];
+        const remaining = target - value;
+        if(map.has(remaining)){
+            return [map.get(remaining), i];
+        }
+        map.set(value, i)
     }
+    return [];
 };
