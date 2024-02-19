8279c6d9d585e6f1c12e79e732012b4b1913eacf
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-02-19 11:17:41 +0200

    Time: 139 ms (58.45%) | Memory: 56.7 MB (44.66%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,11 +3,20 @@
  * @return {boolean}
  */
 
+function countDigits(n) {
+    if (n < 10) {
+        return 1;
+    } else {
+        return 1 + countDigits(Math.floor(n / 10));
+    }
+}
+
+
 function reverseNumber(n) {
     if (n < 10) {
         return n
     }
-    let exp = n.toString().length - 1
+    let exp = countDigits(n) - 1;
     return (n % 10) * 10 ** exp + reverseNumber(Math.floor(n / 10))
 }
 
