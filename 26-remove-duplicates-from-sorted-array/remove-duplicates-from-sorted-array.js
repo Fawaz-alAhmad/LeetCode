ac1257cdf686105785103873d80969c347e7ff92
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-02-20 20:13:59 +0200

    Time: 56 ms (89.61%) | Memory: 51.9 MB (40.53%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,15 +3,9 @@
  * @return {number}
  */
 const removeDuplicates = function (nums) {
-    const set = new Set();
-    for (let i = 0; i < nums.length;) {
-        if (set.has(nums[i])) {
-            nums.splice(i,1)
-        }
-        else {
-            set.add(nums[i])
-             i++
-        }
-    }
-    return nums.length
+  const uniqueNums = new Set(nums);
+
+  nums.splice(0, uniqueNums.size, ...Array.from(uniqueNums));
+  
+  return uniqueNums.size;
 };
